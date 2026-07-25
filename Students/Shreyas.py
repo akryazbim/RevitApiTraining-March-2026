8b7807b85553e987fd580af6216dcb99119f25d3
shreyas <nayak213@gmail.com>
2026-07-25 14:29:50 +0400

    checked for practice if and else

--- Students/Shreyas.py
+++ Students/Shreyas.py
@@ -6,3 +6,15 @@ if ductLength >3000 and ductLength <4000:
     print("pass")
 else:
     print("Fail")
+
+
+marks = 100
+
+if marks >=100:
+    print("your passed with Grade A")
+elif marks >= 75:
+    print("you have passed with first class")
+elif marks >= 50:
+    print("your just passed with average marks")
+else:
+    print("your failed")
